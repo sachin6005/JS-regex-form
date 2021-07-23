e7a42d8d43b4ba5b6393fdaa0e7bfff8974e126f
sachin kumar <merugune.sachin123@gmail.com>
2021-07-23 23:49:52 +0530

    validations are updated using javascript

--- regex.js
+++ regex.js
@@ -1,21 +1,23 @@
+
 var username = document.getElementById("username")
 var usernameErr = document.getElementById("username-error")
 username.addEventListener('input', function(e){
        console.log(e.target.value)
         var pattern = /^[\w]{6,9}$/;
         var inputValue = e.target.value;
-        var valid = pattern.test(inputValue);
-            if(valid){
+        var validUser = pattern.test(inputValue);
+            if(validUser){
               usernameErr.style.display = 'none'
             }
               else{
                usernameErr.style.display = 'block'
             }
+            
     })
 
     var password = document.getElementById("password")
     var passwordErr = document.getElementById("password-error")
-    password.addEventListener('input', function(e){
+     password.addEventListener('input', function(e){
        console.log(e.target.value)
        var pattern1 = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*](?=.*\d)).{8,}$/;
        var passwordValue = e.target.value;
@@ -27,3 +29,10 @@ username.addEventListener('input', function(e){
            passwordErr.style.display = 'block'
        }
     })
+     var submit = document.getElementById('submit')
+    var form = document.getElementById("form")
+    form.addEventListener('submit',function(e){
+        console.log('form is submitted')
+       
+    })
+  
